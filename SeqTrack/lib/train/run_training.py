6fb2939bbd2237264ebd0e973eebd9ea03aa3274
Abdelrahman Ahmed <157038511+Abdo0213@users.noreply.github.com>
2025-11-10 12:57:42 +0200

    added the ability to use cpu

--- SeqTrack/lib/train/run_training.py
+++ SeqTrack/lib/train/run_training.py
@@ -23,7 +23,8 @@ def init_seeds(seed):
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
-    torch.cuda.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
 
@@ -86,10 +87,13 @@ def main():
     args = parser.parse_args()
 
     if args.local_rank != -1:
-        dist.init_process_group(backend='nccl')
-        torch.cuda.set_device(args.local_rank)
+        backend = 'nccl' if torch.cuda.is_available() else 'gloo'
+        dist.init_process_group(backend=backend)
+        if torch.cuda.is_available():
+            torch.cuda.set_device(args.local_rank)
     else:
-        torch.cuda.set_device(0)
+        if torch.cuda.is_available():
+            torch.cuda.set_device(0)
     print("local_rank:", args.local_rank)
 
     if torch.cuda.is_available():
