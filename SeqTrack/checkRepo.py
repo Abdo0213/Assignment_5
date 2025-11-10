fdb0f3e076f9d4bd80faf26584edcda420a631d1
Abdelrahman Ahmed <157038511+Abdo0213@users.noreply.github.com>
2025-11-10 11:35:44 +0200

    changed the path in checkrepo

--- SeqTrack/checkRepo.py
+++ SeqTrack/checkRepo.py
@@ -1,5 +1,5 @@
 from huggingface_hub import list_repo_files
 
-files = list_repo_files("ayamohamed2500/seqtrack-checkpoints", repo_type="model")
+files = list_repo_files("ayamohamed2500/assignment_5", repo_type="model")
 for f in files:
     print(f)
